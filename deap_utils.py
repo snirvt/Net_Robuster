f165ed69e43ed42dd4c14cd66e6813719a051202
snirvit <snirvt@gmail.com>
2022-05-13 06:14:00 +0300

    created activation module and replacment for activations in run time

--- deap_utils.py
+++ deap_utils.py
@@ -14,9 +14,25 @@ setattr(layer_dict[0][0], layer_dict[0][1], nn.SELU())
 # layer_dict[0][2] will contain the original activation function
 
 '''
+# https://stackoverflow.com/questions/58297197/how-to-change-activation-layer-in-pytorch-pretrained-module
 def save_activation_location(model, layer_dict):
     for child_name, child in model.named_children():
         if child._get_name() in dir(torch.nn.modules.activation):
             layer_dict[len(layer_dict)] = [model, child_name, child]
         else:
             save_activation_location(child, layer_dict)
+
+def replace_model_activations(layer_dict, activation_list, activation_idx):
+    for i in range(len(activation_list)):
+        idx = activation_idx[i]
+        setattr(layer_dict[idx][0], layer_dict[idx][1], activation_list[i])
+
+
+class activation_module(torch.nn.Module):
+    def __init__(self, activation):
+        super().__init__()
+        self.activation = activation
+   
+    def __call__(self, x):
+        return self.activation(x)
+
